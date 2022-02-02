51b13fc5989e03e8e7d643aeca5d5c821aaa6333
Venkatraamankrvr <96036116+Venkatraamankrvr@users.noreply.github.com>
2022-02-02 07:39:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-
- const ratingStars = [...document.getElementsByClassName("rating__star")];
-
-function executeRating(stars) {
-  const starClassActive = "rating__star fas fa-star";
-  const starClassInactive = "rating__star far fa-star";
-  const starsLength = stars.length;
-  let i;
-  stars.map((star) => {
-    star.onclick = () => {
-      console.log(star);
-      i = stars.indexOf(star);
-      if (star.className===starClassInactive) 
-      {
-        for (i; i >= 0; --i) 
-        stars[i].className = starClassActive;
-      }
-       else 
-       {
-        for (i; i < starsLength; ++i) 
-        stars[i].className = starClassInactive;
-      }
-    };
-  });
+// variables
+const staricon = document.querySelectorAll(".icon");
+const output = document.querySelector(".result")
+// function to make active state
+function activeState(val) {
+    // console.log(val);
+    for (let i = 0; i <= val; i++) {
+        // console.log(i);
+        staricon[i].classList.add("active");
+        console.log(staricon[i]);
+    }
 }
-executeRating(ratingStars);
+
+staricon.forEach((icon) => {
+    icon.addEventListener('click', (e) => {
+        staricon.forEach((Element) => {
+        Element.classList.remove("active")
+        console.log(Element.id)
+        let res = Number( icon.id ) + 1
+        output.innerHTML = ` ✴️ ${res}Star Ratings`;
+        console.log('removing')}); 
+        activeState(icon.id);
+        output.classList.remove('hidden')
+        output.classList.add('show')
+        console.log(icon.id);
+    });
+});
